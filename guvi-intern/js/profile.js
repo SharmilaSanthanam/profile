28534d59dddf8e8be4408655c8ef0a21e89b3dc3
SharmilaSanthanam <87688938+SharmilaSanthanam@users.noreply.github.com>
2022-10-28 23:32:24 +0530

    Add files via upload

--- guvi-intern/js/profile.js
+++ guvi-intern/js/profile.js
@@ -1,11 +1,34 @@
-$(document).ready(function () {
-    $.ajax({
-        type: "GET",
-        url: "http://localhost/guvi-intern/php/profile.php",
-        dataType: "html",
-        success: function (data) {
-            $("#data").html(data);
-
+$(document).ready(function (){
+    $(document).on("click", "#btn-register", function (e){
+        e.preventDefault();
+        var name=$("#name").val();
+        var mobile=$("#mobile").val();
+        var country=$("#country").val();
+    
+    
+        if(name == "" || mobile == "" || country == "")
+        {
+    $('#msg').html("Please fill the required details")
         }
-    });
-});
+        else{
+            $.ajax({
+                url: 'http://localhost/guvi-intern/php/profile.php',
+                method: 'post',
+                data:
+                                {
+                                    name:name,
+                                    mobile:mobile,
+                                    country:country
+                
+                                },
+                success: function (data) {
+                    $('#msg').html(data);
+                },
+    
+            })
+        }
+    })
+    })
+       
+
+   
